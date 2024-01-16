076d5e57dd9ef658ea5323c239f0eb0368617a07
Javidan Javadov <101976618+javidanjavadov@users.noreply.github.com>
2024-01-16 21:07:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let firstName = 'Cavidan';
-let surName = 'Cavadov';
-let age = 16;
+const surName = 'Cavadov';
+var age = 16;
 let country = 'Baku';
 let allInformations = firstName + ' ' + surName + ' ' + age + ' ' + country
 console.log(allInformations)
