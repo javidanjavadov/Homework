4fc4da596523c2e46a02f631340fc5cc8f0a2c88
Javidan Javadov <101976618+javidanjavadov@users.noreply.github.com>
2024-01-16 16:09:13 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let firstName = 'Cavidan';
+let surName = 'Cavadov';
+let age = 16;
+let country = 'Baku';
+let allInformations = firstName + ' ' + surName + ' ' + age + ' ' + country
+console.log(allInformations)
