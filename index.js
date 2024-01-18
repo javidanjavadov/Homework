6a50152af834b1629132685dd487f820d32611df
Javidan Javadov <101976618+javidanjavadov@users.noreply.github.com>
2024-01-18 16:56:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-let firstName = 'Cavidan';
-const surName = 'Cavadov';
-var age = 16;
-let country = 'Baku';
-let allInformations = firstName + ' ' + surName + ' ' + age + ' ' + country
-console.log(allInformations)
+const employeeDetails = {
+  fullName: "Javadov Javidan",
+  position: "Engineer",
+  yearsOfExperience: 3,
+  skills: ["JavaScript", "HTML", "CSS"],
+  contact: {
+    email: "default@gmail.com",
+    phone: "98239829839822"
+  }
+};
+
+delete employeeDetails.contact.email;
+employeeDetails.department = "Engineering";
+
+const { fullName: employeeName, position, contact, rest } = employeeDetails;
+console.log(employeeName)
+console.log(position);
+console.log(contact);
+console.log(rest);
+
+const projectScores = [
+  { projectId: 1, score: 95 },
+  { projectId: 2, score: 88 },
+];
+
+console.log(projectScores[0].projectId);
+console.log(projectScores[0].score);
+
+const personalInfo = {
+  homeAddress: "18 Street",
+  emergencyContact: "Default",
+  maritalStatus: "Married"
+};
+
+const completeEmployeeProfile = { employeeDetails, personalInfo };
+console.log(completeEmployeeProfile);
