32d83bc1b32a29619a828e2403b3f5fca2320da3
Javidan Javadov <101976618+javidanjavadov@users.noreply.github.com>
2024-01-18 16:51:11 +0400

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,23 +1,29 @@
-const personalProfile = {
-    firstName: 'Cavidan' ,
-    lastName: 'Cavadov' ,
-    age: 16 ,
-    favoriteLanguage: 'English' ,   
- contactInfo: {
-    email : 'javidan.javadov@gmail.com' ,
-    phone : '+994503738788'
-}
-}
-personalProfile['occupation'] = 'Developer';
-personalProfile.contactInfo['address'] = '1245677 Street';
-console.log(personalProfile);
-console.log(personalProfile.firstName);
-console.log(personalProfile.lastName);
-console.log(personalProfile.age);
-console.log(personalProfile.favoriteLanguage);
-console.log(personalProfile.occupation);
-console.log(personalProfile['contactInfo'].email);
-console.log(personalProfile['contactInfo'].phone);
-console.log(personalProfile['contactInfo'].address);
-
-
+const studentProfile = {
+    name: "Javadov Javidan",
+    age: 17,
+    course: "Computer Science",
+    hobbies: ["default1", "default2", "default3"],
+   };
+   
+   delete studentProfile.course;
+   
+   const { name: studentName, age, rest } = studentProfile;
+   
+   console.log(studentName);
+   console.log(age);
+   console.log(rest);
+   
+   const grades = [85, 92, 78, 95];
+   
+   console.log(grades[0]);
+   console.log(grades[1]);
+   
+   const additionalInfo = {
+    address: "493 Street",
+    phoneNumber: "3929832989832",
+   };
+   
+   const completeProfile = { studentProfile, additionalInfo };
+   
+   console.log(completeProfile);
+   
