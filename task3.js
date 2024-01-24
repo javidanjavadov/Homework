8ac9131dfa577665a1a5b6add4c4832fe5db0fe7
Javidan Javadov <101976618+javidanjavadov@users.noreply.github.com>
2024-01-24 11:22:47 +0400

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,16 +1,17 @@
+console.log("Welcome to the voting form. Every nomination and votes for upcoming Presidents are taken into consideration. Please think well and hard before making your decision. We will personally do the consolidation and calculation. ")
 const prompt=require("prompt-sync")({sigint:true}); 
 let name = prompt("Please enter your name: ");
 let surName = prompt("Please enter your surname: ");
 let age = prompt("Please enter your age: ");
 age = parseInt(age);
-if (age >= 18) {
-    console.log("Congratulations! You are old enough to vote.");
-    let presidentChoosement = prompt("Choose a president: ")
-
-    console.log("You are " + name + " " + surName + " and You are " + age + '. ' + "You chose President" + presidentChoosement )  
-      
-  } else
-    console.log("Sorry, you are too young to vote. You must be at least 18 years old.");
-    return;
-  
-
+    if (age >= 18) {
+        console.log("Congratulations! You are old enough to vote.");
+        let presidentChoosement = prompt("Choose a president: ")
+        console.log("You are " + name + " " + surName + " and You are " + age + '. ' + "You chose President" + presidentChoosement )          
+    } else if (isNaN(age)) {
+        console.log("It is not a number. Please enter your age");
+        return age;        
+    } else {
+        console.log("Sorry, you are too young to vote. You must be at least 18 years old.");
+        return;
+    }
